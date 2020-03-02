2f365dbb4b4ec687619849f25bbba921ea30c3cd
Vrijraj Singh <vrijraj2396@gmail.com>
2020-03-02 20:41:29 +0530

    Home Screen Updated

--- src/components/home/HomeStartScreenMobileView.vue
+++ src/components/home/HomeStartScreenMobileView.vue
@@ -3,38 +3,51 @@
   <br>
   <v-card
     flat
-    class="mx-auto my-1 elevation-0"
-    max-width="374"
+    class="mx-auto my-0 elevation-0"
+    max-width=""
     height="90%"
-    style="border:1px solid #EEEEEE;border-radius:8px"
   >
-    <!-- <v-img
-      height="250"
-      :src="require('@/assets/img/main.webp')"
-      :lazy-src="require('@/assets/img/main.webp')"
-    ></v-img> -->
-                <v-img
-                    :src="require('@/assets/img/logo.png')"
-                    :lazy-src="require('@/assets/img/logo.png')"
-                    width="10vh" class="ml-3 mt-5">
-                    <v-layout
-                        slot="placeholder"
-                        fill-height
-                        align-center
-                        justify-center
-                        ma-0
-                    >
-                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                    </v-layout>
-                </v-img>
+    <v-img
+        :src="require('@/assets/img/home2.png')"
+        :lazy-src="require('@/assets/img/home2.png')"
+        width="100%">
+        <v-layout
+            slot="placeholder"
+            fill-height
+            align-center
+            justify-center
+            ma-0
+        >
+            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+        </v-layout>
+    </v-img>
+    <v-img
+        :src="require('@/assets/img/logo.png')"
+        :lazy-src="require('@/assets/img/logo.png')"
+        width="5vh" class="ml-4 mt-5">
+        <v-layout
+            slot="placeholder"
+            fill-height
+            align-center
+            justify-center
+            ma-0
+        >
+            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+        </v-layout>
+    </v-img>
 
-    <v-card-title class="google-font">International Women's Day 2020</v-card-title>
+    <v-card-title class="google-font" style="font-size:180%;color:#0097a7;margin-top:-10px">International Women's Day 2020</v-card-title>
     <v-card-text>
-      <div class="my-0 subtitle-1 black--text google-font">
-        GDG Jalandhar
+      <div class="my-0 black--text google-font mb-5" style="font-size:150%">
+        By GDG WTM Jalandhar
       </div>
-
-      <div class="google-font">International Women's day have been celebrated since years. With devotion to women, we Women Techmakers, Jalandhar along with Google Developers Group, Jalandhar want to accomplish a huge tech revolution with growing women in tech. Since, the era of rationalised culture impacted doting together in the work field.</div>
+    
+      <p class="google-font my-5" >
+          <span style="font-size:120%;background:#0097a7;color:white;border:1px solid #0097a7;border-radius:7px;padding: 8px 70px" class="">
+              #TogetherWeRise
+          </span>
+      </p> 
+      <div class="google-font" style="font-size:110%">International Women's day have been celebrated since years. With devotion to women, we Women Techmakers, Jalandhar along with Google Developers Group, Jalandhar want to accomplish a huge tech revolution with growing women in tech. Since, the era of rationalised culture impacted doting together in the work field.</div>
     </v-card-text>
 
     <v-divider class="mx-4"></v-divider>
